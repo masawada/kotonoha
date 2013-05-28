d1d85277c48b445b1512da6779f120f69c74dc23
Masayoshi Wada <developer@andantesoftware.com>
2013-05-28 13:56:01 +0900

    created /statuses/destroy/:id

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -26,5 +26,30 @@ module Kotonoha
         }
       }
     end
+
+    def self.destroy(auth, queries)
+      begin
+        user = Models::User.find(auth.user.id)
+      rescue ActiveRecord::RecordNotFound
+        raise ERR::UserNotFound
+      end
+
+      raise ERR::BadRequest if queries["captures"].first.nil?
+
+      begin
+        leaf = Models::Leaf.find(queries["captures"].first.to_i)
+      rescue ActiveRecord::RecordNotFound
+        raise ERR::LeafNotFound
+      end
+
+      raise ERR::RequestForbidden unless leaf.user.id == user.id
+      
+      deleted_id = leaf.id
+      leaf.destroy
+
+      {
+        deleted_id: deleted_id
+      }
+    end
   end
 end
