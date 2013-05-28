6c0bad81dc320e553c7ab7592d6f775e91f1b5f7
Masayoshi Wada <developer@andantesoftware.com>
2013-05-28 14:03:04 +0900

    created /statuses/timeline

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -47,9 +47,39 @@ module Kotonoha
       deleted_id = leaf.id
       leaf.destroy
 
-      {
-        deleted_id: deleted_id
-      }
+      { deleted_id: deleted_id }
+    end
+
+    def self.timeline(queries)
+      max_id = queries["max_id"].to_i
+      since_id = queries["since_id"].to_i
+      count = queries["count"].to_i
+      count = 20 if count.nil?
+
+      begin
+        leaves = Models::Leaf.order('id desc')
+        leaves = leaves.where('id < :max_id', {:max_id => max_id}) unless max_id.nil?
+        leaves = leaves.where('id < :since_id', {:since_id => since_id}) unless since_id.nil?
+      rescue ActiveRecord::RecordNotFound
+        raise ERR::LeafNotFound
+      rescue
+        raise ERR::InternalServerError
+      end
+
+      response_array = {}
+
+      leaves.each do |leaf|
+        response_array[leaf.id] = {
+          id: leaf.id,
+          text: leaf.text,
+          user_id: leaf.user.id,
+          user_name: leaf.user.name,
+          favorites: leaf.favorites,
+          timestamp: leaf.created_at.to_i
+        }
+      end
+
+      { statuses: response_array }
     end
   end
 end
